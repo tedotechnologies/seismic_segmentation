01f12bf510c21e38442a7b98e348c8dc51fbdf9f
Daniel <daniel.matveev@tedo.com>
2025-04-01 17:02:21 +0000

    add settings

---
+++ experiments/settings.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLEARML_WEB_HOST = os.getenv("CLEARML_WEB_HOST")
+CLEARML_API_HOST = os.getenv("CLEARML_API_HOST")
+CLEARML_FILES_HOST = os.getenv("CLEARML_FILES_HOST")
+CLEARML_API_ACCESS_KEY = os.getenv("CLEARML_API_ACCESS_KEY")
+CLEARML_API_SECRET_KEY = os.getenv("CLEARML_API_SECRET_KEY")
--- run_train.sh
+++ run_train.sh
@@ -2,4 +2,5 @@
 
 SCRIPT_PATH="/home/dmatveev/workdir/rosneft_segmentation/experiments/train.py"
 
-python3 "$SCRIPT_PATH"
+# python3 "$SCRIPT_PATH"
+python3 "$SCRIPT_PATH" --multirun experiments=exp1,exp2,exp3,exp4,exp5,exp6
